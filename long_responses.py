2f2755c22a53d7adab59ad501f4e0f5cecb17d64
neto <neto3421fg@gmail.com>
2021-09-03 16:51:03 -0300

    bot

---
+++ long_responses.py
@@ -0,0 +1,9 @@
+import  random
+
+R_EATING = "I don´t like eating anything because I´m a bot obviously!"
+
+def unknown():
+    response = ["Could you please re-phrase that?",
+                "...",
+                "What does that mean?"][random.randrange(4)]
+    return response
